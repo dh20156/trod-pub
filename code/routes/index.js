cf6c859a57d9ad2c1ea6d74248f4a9d0471f0bac
dh20156 <dh20156@gmail.com>
2022-06-07 14:14:55 +0800

    update meta info

--- code/routes/index.js
+++ code/routes/index.js
@@ -3,8 +3,8 @@ const router = require("koa-router")();
 router.get("/", async (ctx, next) => {
   await ctx.render("index", {
     title: "技术风险开放数据 | TROD",
-    meta-keywords: "trod,首页",
-    meta-description:"TROD,技术风险开放数据"
+    keywords: "trod,首页",
+    description:"TROD,技术风险开放数据"
   });
 });
 
