ac640a3039501c7309c44ac41d2b953cfcb0ddbd
dh20156 <dh20156@gmail.com>
2022-06-17 17:32:44 +0800

    Update index.js

--- code/routes/index.js
+++ code/routes/index.js
@@ -1,21 +1,12 @@
 const router = require("koa-router")();
 
 router.get("/", async (ctx, next) => {
-  await ctx.render("index", {
-    title: "技术风险开放数据 | TROD",
-    keywords: "trod,首页",
-    description:"TROD,技术风险开放数据"
-  });
-});
-
-router.get("/string", async (ctx, next) => {
-  ctx.body = "koa2 string";
-});
-
-router.get("/json", async (ctx, next) => {
-  ctx.body = {
-    title: "koa2 json",
-  };
+    await ctx.render("index", {
+        title: "IT系统稳定性技术风险开放数据",
+        keywords: "JVM参数配置,JDK缺陷故障,服务器端口配置,数据库连接池配置,日志框架异步配置,缓存配置,文件句柄占用,HashMap并发,Netty死循环,Mybatis框架静态SQL不规范,System.gc,同步锁竞争",
+        description:"IT系统稳定性技术风险开放数据是一个开源项目，收集了大量实践中遇到的JVM参数配置、JDK缺陷故障、服务器端口配置、数据库连接池配置、日志框架异步配置、缓存配置、文件句柄占用、HashMap并发、Netty死循环、Mybatis框架静态SQL不规范、System.gc、同步锁竞争等风险诊断项并详细介绍了问题发生的原因及解决方案",
+        canonical:"https://trod.pub/"
+    });
 });
 
 module.exports = router;
